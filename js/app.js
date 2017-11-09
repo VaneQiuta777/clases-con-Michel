b19b8e8d021356de89c94efe0404f783073437eb
VaneQiuta777 <vanessa.colsan@gmail.com>
2017-11-09 18:25:26 -0500

    subiendo clases de Michel

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById("input-sensible").addEventListener("keyup", function() {
+    var divColor = document.querySelector("div");
+        divColor.classList.toggle ("yellow");
+
+    }
+);
+
